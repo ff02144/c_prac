d114cbd61138df622fa2222da639f1879230e0a3
ff02144 <ff021144050992@gmail.com>
2026-07-31 19:54:11 +0800

    render_display()

--- chip8/display.c
+++ chip8/display.c
@@ -1,5 +1,6 @@
 #include "display.h"
 
+static SDL_Texture *texture = NULL;
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
 
@@ -31,12 +32,49 @@ SDL_DestroyWindow(window);
 SDL_Quit();
 return 1;
 }
+texture=SDL_CreateTexture(
+renderer,
+SDL_PIXELFORMAT_ARGB8888,
+SDL_TEXTUREACCESS_STREAMING,
+64,
+32);
+
+if(texture==NULL){
+printf("SDL_CreateTexture錯誤:%s\n",SDL_GetError());
+SDL_DestroyRenderer(renderer);
+SDL_DestroyWindow(window);
+SDL_Quit();
+return 1;
+}
  return 0;
 }
 
-void close_display(void){
 
-}
+
+
+void close_display(void){}
+
 
 void render_display(uint8_t display[32][64]){
+void *pixels;
+int pitch;
+if(SDL_LockTexture(texture,NULL,&pixels,&pitch)!=0){
+printf("SDL_LockTexture錯誤:%s\n",SDL_GetError());
+return;
+}
+uint32_t *pixel_data=(uint32_t *)pixels;
+int row_pixels=pitch/4;
+
+for(int y=0;y<32;y++){
+for(int x=0;x<64;x++){
+uint32_t color=display[y][x] ? 0xFFFFFFFF:0xFF000000;
+pixel_data[y*row_pixels+x]=color;
+}
+}
+
+SDL_UnlockTexture(texture);
+
+SDL_Rect destRect={0,0,960,480};
+SDL_RenderCopy(renderer,texture,NULL,&destRect);
+SDL_RenderPresent(renderer);
 }
